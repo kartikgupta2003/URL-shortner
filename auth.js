adfdd909163e245883fd16a634f896f46ede95ae
kartikgupta2003 <133004844+kartikgupta2003@users.noreply.github.com>
2025-05-11 11:11:03 +0530

    Add files via upload

--- auth.js
+++ auth.js
@@ -1,15 +1,50 @@
-const sessionIdToUserMap = new Map();
-// we have created a hash map
+const {getUser} = require("../service/auth.js");
+async function restrictToLoggedinUserOnly(req , res , next){
+    console.log(req.cookies);
+    const userUid = req.cookies?.uid;
+//     // because uid is the name given to cookies
+//     // pehle ye check kar rha ki cookies hai , fir unme se uid ko parse karega
+//     // otherwise ye not defined ko parse karne ki koshish karega to diikat ho jayegi 
+console.log(userUid);
+    if(!userUid) console.log("bhasad");
+    if(!userUid) return res.redirect('/login');
+//     // wo dubara dubara render kyu kare ab to redirect karo
+    const user = getUser(userUid);
+    console.log(user);
+    if(!user) return res.redirect("/login");
 
-function setUser(id , user){
-    sessionIdToUserMap.set(id , user);
+    req.user = user;
+    next();
 }
 
-function getUser(id){
-    return sessionIdToUserMap.get(id);
+async function checkAuth(req , res , next){
+    const userUid = req.cookies?.uid;
+    const user = getUser(userUid);
+    req.user = user;
+    next();
 }
 
-module.exports = {
-    setUser,
-    getUser,
-}
+module.exports={
+    restrictToLoggedinUserOnly,
+    checkAuth,
+};
+
+
+// When a client (like a browser) sends a request to your server, it includes all cookies in a single header like this:
+
+// Cookie: uid=abc123; theme=dark; rememberMe=true
+// This whole thing is just one string in the request — and not automatically parsed by Express.
+
+// So what does cookie-parser do?
+// It parses that raw cookie string and gives you a nice JavaScript object like this:
+
+// req.cookies = {
+//   uid: "abc123",
+//   theme: "dark",
+//   rememberMe: "true"
+// };
+// Then you can simply access what you want:
+
+// const userUid = req.cookies.uid;
+
+// cookie-parser is an Express middleware that parses the Cookie header in incoming requests and populates req.cookies with a JavaScript object
