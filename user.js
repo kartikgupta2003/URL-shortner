96fec52efd469366e6fa76c35ac9c3fa33882a27
kartikgupta2003 <133004844+kartikgupta2003@users.noreply.github.com>
2025-05-11 11:25:12 +0530

    Delete user.js

--- user.js
+++
@@ -1,6 +0,0 @@
-const express= require("express");
-const {handleUserSignup , handleUserSignin} = require("../controllers/user.js");
-const router = express.Router();
-router.post("/" , handleUserSignup);
-router.post("/login" , handleUserSignin);
-module.exports = router;
