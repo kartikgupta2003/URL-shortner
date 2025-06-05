b5c770319d6a85f35a4fb293529533ee829946f7
kartikgupta2003 <133004844+kartikgupta2003@users.noreply.github.com>
2025-06-05 15:11:44 +0530

    Delete routes/routes_url.js

--- routes/routes_url.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const {handlegenerateNewShortURL , handleRedirect , handleAnalytics} = require("../controllers/controllers_url.js");
-
-router.post("/" , handlegenerateNewShortURL);
-router.get("/:shortId" , handleRedirect);
-router.get("/analytics/:shortId" , handleAnalytics);
-
-module.exports = router ;
